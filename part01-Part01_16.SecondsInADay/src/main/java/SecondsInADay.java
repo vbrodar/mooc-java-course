8c9a70898b1036c97f5be6f3060de2193f0a97b2
vbrodar <vedran.brodar@hotmail.com>
2021-05-12 08:08:47 +0100

    Finished Part 1-5.Calculating with numbers

--- part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -7,6 +7,9 @@ public class SecondsInADay {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+         System.out.println("How many days would you like to convert to seconds?");
+         int number = Integer.valueOf(scanner.nextLine());
+         int sum = number * 60 * 60 * 24;
+         System.out.println(sum);
     }
 }
--- part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -7,6 +7,17 @@ public class SimpleCalculator {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        int sum = first + second;
+        int difference = first - second;
+        int multiplied = first * second;
+        double divided = 1.0 * first / second;
+        System.out.println(first+ " + " +second+ " = " +sum);
+        System.out.println(first+ " - " +second+ " = " +difference);
+        System.out.println(first+ " * " +second+ " = " +multiplied);
+        System.out.println(first+ " / " +second+ " = " +divided);
     }
 }
