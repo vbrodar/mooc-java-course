d55b3c32b242f414035eeb0ac93336ae5a3b57d9
vbrodar <vedran.brodar@hotmail.com>
2021-05-12 09:42:50 +0100

    Finished Part 1 completely

--- part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -5,6 +5,12 @@ public class LeapYear {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int leapYear = Integer.valueOf(scan.nextLine());
+        if ((leapYear % 4 == 0) && (leapYear % 100 != 0) || (leapYear % 400 == 0)) {
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
 
     }
 }
